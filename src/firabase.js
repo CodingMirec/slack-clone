4046e99bfb3c43e6cbc141954acfe01c0cb59726
CodingMirec <m.sykora01@gmail.com>
2021-03-02 21:12:43 +0100

    added firebase

---
+++ src/firabase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDlEWF3400N76t3PnlzRNFa4whflo0-g8o",
+  authDomain: "slack-clone-ms-6a2eb.firebaseapp.com",
+  projectId: "slack-clone-ms-6a2eb",
+  storageBucket: "slack-clone-ms-6a2eb.appspot.com",
+  messagingSenderId: "803335895290",
+  appId: "1:803335895290:web:c2cc9a63822ed45844b143",
+  measurementId: "G-1NQYLZV2MQ",
+};
