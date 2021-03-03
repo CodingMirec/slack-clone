ce2f0e92b127f80055ddb3780975ce43fcb22397
CodingMirec <m.sykora01@gmail.com>
2021-03-03 20:31:25 +0100

    firebase db, auth

--- src/firabase.js
+++ src/firabase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDlEWF3400N76t3PnlzRNFa4whflo0-g8o",
   authDomain: "slack-clone-ms-6a2eb.firebaseapp.com",
@@ -7,3 +9,10 @@ const firebaseConfig = {
   appId: "1:803335895290:web:c2cc9a63822ed45844b143",
   measurementId: "G-1NQYLZV2MQ",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
