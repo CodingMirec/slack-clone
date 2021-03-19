6c3b6e38a4c7a60bcd48d15396d55015315557da
CodingMirec <m.sykora01@gmail.com>
2021-03-19 10:42:55 +0100

    line deleted

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ function App() {
             src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
             alt="logo"
           />
-
           <Spinner name="ball-spin-fade-loader" color="purple" fadeIn="none" />
         </AppLoadingContents>
       </AppLoading>
